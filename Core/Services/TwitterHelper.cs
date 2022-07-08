d66979dcd3bd4fcc2dd311d154612b372fbff603
Owain <owilliams@spindogs.com>
2022-07-08 17:05:42 +0100

    IMP: Way to run the site without needing API keys
    With the API.Offline mode, you can now run the site without needing any API keys

--- Core/Services/TwitterHelper.cs
+++ Core/Services/TwitterHelper.cs
@@ -14,45 +14,46 @@ namespace H5YR.Core.Services
     {
         private readonly ILogger<TwitterHelper> logger;
         private readonly IOptions<TwitterSettings> settings;
+        private readonly IOptions<APISettings> apiSettings;
 
-        public TwitterHelper(ILogger<TwitterHelper> logger, IOptions<TwitterSettings> settings)
+        public TwitterHelper(ILogger<TwitterHelper> logger, IOptions<TwitterSettings> settings, IOptions<APISettings> apiSettings)
         {
             this.logger = logger;
             this.settings = settings;
+            this.apiSettings = apiSettings;
         }
 
         public List<TweetModel> GetAllTweets(int tweetsToSkip, int tweetsToReturn)
         {
-            // You need to make sure your app on dev.twitter.com has read and write permissions if you wish to tweet!
-            var creds = new TwitterCredentials(settings.Value.ConsumerKey, settings.Value.ConsumerSecret, settings.Value.AccessToken, settings.Value.AccessTokenSecret);
-            var userClient = new TwitterClient(creds);
 
-            var searchResults = userClient.Search.SearchTweetsAsync("#h5yr");
-
-
-            List<TweetModel> FetchTweets = new List<TweetModel>();
-
-
-            foreach (var tweet in searchResults.Result.Skip(tweetsToSkip).Take(tweetsToReturn))
+            List<TweetModel> Tweets = new List<TweetModel>();
+            if (apiSettings.Value.Offline == null || apiSettings.Value?.Offline?.ToLowerInvariant() != "true")
             {
-                FetchTweets.Add(new TweetModel()
+                var creds = new TwitterCredentials(settings.Value.ConsumerKey, settings.Value.ConsumerSecret, settings.Value.AccessToken, settings.Value.AccessTokenSecret);
+                var userClient = new TwitterClient(creds);
+
+                var searchResults = userClient.Search.SearchTweetsAsync("#h5yr");
+
+                foreach (var tweet in searchResults.Result.Skip(tweetsToSkip).Take(tweetsToReturn))
                 {
+                    Tweets.Add(new TweetModel()
+                    {
 
-                    Username = tweet.CreatedBy.ToString(),
-                    Avatar = tweet.CreatedBy.ProfileImageUrl,
-                    Content = tweet.Text,
-                    ScreenName = tweet.CreatedBy.ScreenName.ToString(),
-                    TweetedOn = tweet.CreatedAt,
-                    NumberOfTweets = FetchTweets.Count(),
-                    ReplyToTweet = tweet.IdStr,
-                    Url = tweet.Url
+                        Username = tweet.CreatedBy.ToString(),
+                        Avatar = tweet.CreatedBy.ProfileImageUrl,
+                        Content = tweet.Text,
+                        ScreenName = tweet.CreatedBy.ScreenName.ToString(),
+                        TweetedOn = tweet.CreatedAt,
+                        NumberOfTweets = Tweets.Count(),
+                        ReplyToTweet = tweet.IdStr,
+                        Url = tweet.Url
 
-                });
+                    });
 
+                };
+            }
 
-            };
-
-            return FetchTweets;
+            return Tweets;
         }
 
     }
--- ViewComponents/TweetsViewComponent.cs
+++ ViewComponents/TweetsViewComponent.cs
@@ -1,17 +1,23 @@
-﻿using H5YR.Core.Services;
+﻿using h5yr.Settings;
+using H5YR.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using System.Text.Json;
 
 namespace h5yr.ViewComponents
 {
     public class TweetsViewComponent : ViewComponent
     {
         private readonly ITwitterHelper twitterHelper;
+        private readonly IOptions<APISettings> apiSettings;
+      private readonly ILogger<TweetsViewComponent> logger;
 
-        public TweetsViewComponent(ITwitterHelper twitterHelper)
+
+        public TweetsViewComponent(ITwitterHelper twitterHelper, IOptions<APISettings> apiSettings, ILogger<TweetsViewComponent> logger)
         {
             this.twitterHelper = twitterHelper;
+            this.apiSettings = apiSettings;
+            this.logger = logger;
         }
 
         public IViewComponentResult Invoke(string loadmore = "false")
@@ -19,25 +25,48 @@ namespace h5yr.ViewComponents
 
             HttpContext.Session.SetInt32("NumberOfTweetsDisplayed", 12);
 
-            List<TweetModel> tweets;
+            List<TweetModel> tweet = new();
 
-            tweets = twitterHelper.GetAllTweets(0, 12);
+            if(apiSettings.Value.Offline == null || apiSettings.Value.Offline.ToLowerInvariant() != "true")
+            {
+                tweet = twitterHelper.GetAllTweets(0, 12);
 
-            //  var json = JsonConvert.SerializeObject(tweets);
-            //  string fileName = "TestTweets.json";
-            //  string jsonString = System.Text.Json.JsonSerializer.Serialize(tweets);
-            //  File.WriteAllText(fileName, jsonString);
+                // You can only create a new TestTweets file if you have
+                // a valid Twitter API key setup
+                if (apiSettings.Value.CreateOfflineFile != null || apiSettings.Value.CreateOfflineFile?.ToLowerInvariant() == "true")
+                {
+                    try
+                    {
+                        var json = JsonConvert.SerializeObject(tweet);
+                        string fileName = "TestTweets.json";
+                        string jsonString = System.Text.Json.JsonSerializer.Serialize(json);
+                        File.WriteAllText(fileName, jsonString);
+                    }
+                    catch(Exception ex)
+                    {
+                        logger.LogError("Error: Unable to write Test Tweets Json file", ex);
+                    }
+                    
 
-            string fileName = "TestTweets.json";
-            string jsonString = File.ReadAllText(fileName);
-            List<TweetModel> tweet = System.Text.Json.JsonSerializer.Deserialize<List<TweetModel>>(jsonString)!;
-
+                }
+            }
+            else
+            {
+                try
+                {
+                    string fileName = "TestTweets.json";
+                    string jsonString = File.ReadAllText(fileName);
+                    tweet = System.Text.Json.JsonSerializer.Deserialize<List<TweetModel>>(jsonString)!;
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("Error: Unable to read Tweet Json file", ex);
+                }
+                
+            }
 
             return View(tweet);
         }
-
-
-        
 
     }
 }
