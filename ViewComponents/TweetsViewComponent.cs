f941e3bdb24af70fc4eacaa9ba1690afd4f78f16
Owain <owaingdwilliams@gmail.com>
2022-07-08 09:00:18 +0100

    API Settings / Test mode

--- ViewComponents/TweetsViewComponent.cs
+++ ViewComponents/TweetsViewComponent.cs
@@ -1,5 +1,7 @@
 ﻿using H5YR.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace h5yr.ViewComponents
 {
@@ -21,7 +23,17 @@ namespace h5yr.ViewComponents
 
             tweets = twitterHelper.GetAllTweets(0, 12);
 
-            return View(tweets);
+            //  var json = JsonConvert.SerializeObject(tweets);
+            //  string fileName = "TestTweets.json";
+            //  string jsonString = System.Text.Json.JsonSerializer.Serialize(tweets);
+            //  File.WriteAllText(fileName, jsonString);
+
+            string fileName = "TestTweets.json";
+            string jsonString = File.ReadAllText(fileName);
+            List<TweetModel> tweet = System.Text.Json.JsonSerializer.Deserialize<List<TweetModel>>(jsonString)!;
+
+
+            return View(tweet);
         }
 
 
