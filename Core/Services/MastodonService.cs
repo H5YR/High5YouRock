2aefd469bc95c03c0df90e58b6b18e82861af7ab
Anders Bjerner <abjerner@skybrud.dk>
2023-10-01 13:27:17 +0200

    Replaced home view tweets with Mastodon statuses

---
+++ Core/Services/MastodonService.cs
@@ -0,0 +1,59 @@
+﻿using Skybrud.Social.Mastodon;
+using Skybrud.Social.Mastodon.Models.Statuses;
+using Skybrud.Social.Mastodon.Options.Timeline;
+using Skybrud.Social.Mastodon.Responses.Statuses;
+
+namespace h5yr.Core.Services {
+
+    public class MastodonService : IMastodonService
+    {
+
+        private readonly ILogger<MastodonService> _logger;
+
+        public MastodonService(ILogger<MastodonService> logger)
+        {
+            _logger = logger;
+        }
+
+        public IReadOnlyList<MastodonStatus> GetStatuses(int limit, string? maxId = null)
+        {
+
+            // Initialize a new HTTP service (basically the API wrapper)
+            MastodonHttpService mastodon = MastodonHttpService
+                .CreateFromDomain("umbracocommunity.social");
+
+            // Initialize the options for the request to the API
+            MastodonGetHashtagTimelineOptions options = new()
+            {
+                Hashtag = "h5yr",
+                Limit = limit,
+                Local = true
+            };
+
+            try
+            {
+
+                // Make the request to the API
+                MastodonStatusListResponse response = mastodon
+                    .Timelines
+                    .GetHashtagTimeline(options);
+
+                // Return the statuses
+                return response.Body;
+
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "Failed fetching statuses from the Mastodon API.");
+
+            }
+
+
+            return Array.Empty<MastodonStatus>();
+
+        }
+
+    }
+
+}
---
+++ ViewComponents/MastodonViewComponent.cs
@@ -0,0 +1,66 @@
+﻿using h5yr.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using h5yr.Settings;
+using Microsoft.Extensions.Options;
+using Skybrud.Essentials.Json.Newtonsoft;
+using Skybrud.Social.Mastodon.Models.Statuses;
+
+namespace h5yr.ViewComponents;
+
+public class MastodonViewComponent : ViewComponent {
+
+    private readonly ILogger<MastodonViewComponent> _logger;
+    private readonly IOptions<APISettings> _apiSettings;
+    private readonly IMastodonService _mastodonService;
+
+    protected bool IsOffline => _apiSettings.Value.Offline == "true";
+
+    protected bool CreateOfflineFile => _apiSettings.Value.CreateOfflineFile == "true";
+
+    public MastodonViewComponent(ILogger<MastodonViewComponent> logger, IOptions<APISettings> apiSettings, IMastodonService mastodonService) {
+        _logger = logger;
+        _apiSettings = apiSettings;
+        _mastodonService = mastodonService;
+    }
+
+    public IViewComponentResult Invoke() {
+
+        // Get the statuses
+        IReadOnlyList<MastodonStatus> statuses = GetStatuses();
+
+        // Initialize a new model for the view component
+        MastodonModel model = new(statuses);
+
+        // Return the view
+        return View(model);
+
+    }
+
+    private IReadOnlyList<MastodonStatus> GetStatuses() {
+
+        string fileName = "TestStatuses.json";
+
+        if (IsOffline) {
+            try {
+                return JsonUtils.LoadJsonArray(fileName, MastodonStatus.Parse);
+            } catch (Exception ex) {
+                _logger.LogError(ex, "Error: Unable to read test statuses JSON file");
+                return Array.Empty<MastodonStatus>();
+            }
+        }
+
+        IReadOnlyList<MastodonStatus> statuses = _mastodonService.GetStatuses(12);
+
+        if (CreateOfflineFile) {
+            try {
+                JsonUtils.SaveJsonArray(fileName, statuses);
+            } catch (Exception ex) {
+                _logger.LogError(ex, "Error: Unable to write test statuses JSON file");
+            }
+        }
+
+        return statuses;
+
+    }
+
+}
